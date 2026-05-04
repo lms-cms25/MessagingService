4a700254b01737e7eed3c15594536c9dec6e8b64
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-05-04 22:42:15 +0200

    figuring out message model - not finished

---
+++ src/MessagingService.Domain/Entities/Message.cs
@@ -0,0 +1,21 @@
+﻿namespace MessagingService.Domain.Entities;
+
+public class Message
+{
+    public string Id { get; set; } = null!;
+    public string UserId { get; set; } = null!;
+    public string LiveClassId { get; set; } = null!;
+    public string Text { get; set; } = null!;
+    public bool IsQuestion { get; set; }
+    public bool IsAnswered { get; set; }
+    public MessageLike[]? Like { get; set; } = null!;
+    public string ProfileName { get; set; } = null!;
+    public DateTime CreatedAtUtc { get; set; }
+
+}
+
+public class MessageLike
+{
+    public string UserId { get; set; } = null!;
+    public string ProfileName { get; set; } = null!;
+}
