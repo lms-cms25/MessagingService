4a04d7f6b3237a68fadb4d83613535da1c2e5e3c
migue <migue@LAPTOP-LIQQFHEB>
2026-05-05 14:21:36 +0200

    Messaging back and forward done

--- src/MessagingService.Api/Program.cs
+++ src/MessagingService.Api/Program.cs
@@ -1,4 +1,5 @@
 using MessagingService.Api.Endpoints;
+using MessagingService.Api.Hubs;
 using MessagingService.Api.OpenApi;
 using MessagingService.Api.Security;
 
@@ -6,14 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCorsConfiguration();
 builder.Services.AddOpenApiConfiguration();
+builder.Services.AddSignalR();
 
 
 
 var app = builder.Build();
 
 app.UseCors("All");
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
+app.MapHub<ChatHub>("/chathub");
 app.MapOpenApiEndpoints();
 app.MapMessagingEndpoints();
 
